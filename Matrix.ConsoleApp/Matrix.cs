3004eeb87d668143a0380ab9ba02c196be98b017
Allkman <algirdas.cern@gmail.com>
2021-12-07 16:03:44 +0200

    Implemented first logic to Matrix Task

--- Matrix.ConsoleApp/Matrix.cs
+++ Matrix.ConsoleApp/Matrix.cs
@@ -3,18 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Task 1. A diagonal matrix is a square matrix, all elements of which outside the main diagonal 
+/// are equal to zero (read here Diagonal matrix - https://en.wikipedia.org/wiki/Diagonal_matrix).
+/// You need to create and test a class to represent a diagonal matrix containing integers (int).
+/// 1. The class object stores only the elements of the matrix located on the diagonal. For this, a one-dimensional array is used. 
+/// 2.  The class object has a read-only property Size - the size of the matrix (for example, for a 5x5 matrix, the Size property returns 5).
+/// 3. The class has a constructor for creating a matrix. A list of parameters (params) is passed to the constructor - these are the elements 
+/// of the matrix located on the diagonal. If the value of the constructor argument is not correct (for example, equal to null),
+/// a zero-size matrix is created (Size = 0). 
+/// 4.  For convenience, the class offers an indexer with
+/// two indices i and j. If i is not equal to j, then the indexer returns the value 0 (when writing, nothing happens).
+/// If the values of the indices are not correct (out of bounds: less than zero or greater or equal to Size),
+/// nothing happens during writing, and when reading, the value 0 is returned. (In this case an exception has to be generated,
+/// but up to now this point wasn’t discussed). 5. The class of the matrix has an instance method Track (),
+/// which returns the sum of the elements of the matrix located on the main diagonal. 
+/// 6.  Override the Equals () and ToString () methods in the matrix class. Two matrices are considered equal
+/// if their sizes and the corresponding elements on the diagonal coincide. 
+/// 7.  Create a diagonal matrix extension method that adds two diagonal matrices. The result of the method is a new diagonal matrix.
+/// If the dimensions of the matrix do not match, the smaller matrix is padded with zeros.
+/// </summary>
 namespace Matrix.ConsoleApp
 {
-    internal class Matrix : IMatrix
+    internal class Matrix
     {
-        public int Size { get; } = 5;
+        //1.
+        private int[] _diagonalMatrixArray;
+        //2.
+        public int Size => _size;
         private readonly int i;
         private readonly int j;
-        public Matrix()
-        {
+        private readonly int _size;
 
+        /// <summary>
+        /// example of 3x3 diagonal matrix
+        /// [(i==j), (i!=j), (i!=j)] (0,0)
+        /// [(i!=j), (i==j), (i!=j)] (1,1)
+        /// [(i!=j), (i!=j), (i==j)] (2,2)
+        /// </summary>
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/passing-arrays-as-arguments
+        //https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/params
+        //passing a a single-dimentional array of params as in ms.docs
+        public Matrix(params int[] diagonalMatrixArray)
+        {
+            _diagonalMatrixArray = diagonalMatrixArray;
+            //3.validating if params element is null
+            //https://stackoverflow.com/questions/6584131/c-sharp-is-it-possible-to-have-null-params @Joshua Rodgers answer
+            if (diagonalMatrixArray == null)
+            {
+                _size = 0; // if any element of array is null I set array size to 0
+            }
+            else
+            {
+             //_size = //some length
+            }
         }
+
+
         private int Track()
         {
             int sum = 0;
--- Matrix.ConsoleApp/Program.cs
+++ Matrix.ConsoleApp/Program.cs
@@ -6,7 +6,12 @@ namespace Matrix.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            var matrixArray = new int[] { 4, 5, 6, 1, 5 };
+            var matrix = new Matrix(matrixArray);
+            for (int i = 0; i < matrix.; i++)
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
