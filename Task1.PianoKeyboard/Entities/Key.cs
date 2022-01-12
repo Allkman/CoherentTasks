f84a1b4499c5fbe8d8d086003df5610723e3ad91
Allkman <algirdas.cern@gmail.com>
2022-01-12 14:51:59 +0200

    Completed refactoring Task1.PianoKey

--- Task1.PianoKeyboard/Entities/Key.cs
+++ Task1.PianoKeyboard/Entities/Key.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Task1.PianoKeyboard.Enums;
+﻿using Task1.PianoKeyboard.Enums;
 
 namespace Task1.PianoKeyboard.Entities
 {
@@ -25,167 +19,51 @@ namespace Task1.PianoKeyboard.Entities
         }
         public int CompareTo(Key other)
         {
-            if (this._octave - other._octave < -1) return 1;
-            if (this._octave - other._octave > 1) return -1;
-            if (this._octave == EOctave.SubContra)
+            //should I implement logic for 2 first Keys from SubContra and 1 last Key from Fifth octaves?
+            if (this._octave == EOctave.SubContra && other._octave == EOctave.SubContra)
             {
                 return ComparingNotesOfSubContraOctave(other);
             }
-            if (other._octave == EOctave.Fifth)
+            if (this._octave == EOctave.Fifth && this._note == ENote.C &&
+                other._octave == EOctave.Fifth && other._note == ENote.C)
             {
                 return ComparingNoteOfFifthOctave(other);
-            }            
-            // when two NEIGHBOUR octave NOTES (B and C) are compared
-            if (this._octave - other._octave == -1 || this._octave - other._octave == 1)
-            {
-                return ComparingTwoNotesFromNeighbourOctaves(other);
             }
-            else
+            //checking if comparable keys are in the same octave OR || if they are in two neighboring octaves && 
+            if ((this._octave == other._octave || this._octave - other._octave == -1) &&
+                //if notes have black Key between them OR || no black Key OR || notes are from neighboring octaves
+                (this._note - other._note == -2 || this._note - other._note == -1 || this._note - other._note == 11))
             {
-                if ((this._note - other._note) == -2 || (this._note - other._note) == 2)
-                {
-                    return ComparingTwoNotesWithBlackKeyBetweenThem(other);
-                }
-                if((this._note - other._note) == -1 || (this._note - other._note) == 1 )
-                {
-                    return ComparingTwoNotes(other);
-                }
-                if ((this._note - other._note) == 0)
-                {  
-                    return ComparingTwoSameNotes(other);
-                }                
-                //           D  follows    F
-                if ((this._note - other._note) < -1) return 1;
-                // F precedes  D
-                return -1;
+                //if Key is black its == 2 OR || 
+                if (this._accidental - other._accidental == 2 ||
+                    //if there is no Key between its == 1
+                    this._accidental - other._accidental == 1) return 0;
+                return 1;
             }
+            return 1;
         }
         private readonly int ComparingNoteOfFifthOctave(Key other)
         {
-            if (other._octave == EOctave.Fifth)
+            if (other._octave == EOctave.Fifth && other._note == ENote.C)
             {
-                if (other._note == ENote.C)
+                if (other._accidental == EAccidental.Sharp)
                 {
-                    if (other._accidental == EAccidental.Sharp)
-                    {
-                        Console.WriteLine("C8 is 88th Key, it cannot have Sharp Note");
-                    }
-                    if (this._accidental - other._accidental == 1) return 0;
+                    Console.WriteLine("C8 is 88th Key, it cannot have Sharp Accidental");
                 }
+                if (this._accidental - other._accidental == 0) return 0;
                 else Console.WriteLine("Cannot compare non existing Key");
             }
             return 1;
         }
         private readonly int ComparingNotesOfSubContraOctave(Key other)
         {
-            if (this._octave == EOctave.SubContra)
+            if (this._note == ENote.A && other._note == ENote.B)
             {
-                if (this._note == ENote.A && other._note == ENote.B)
-                {
-                    //           A   follows   B
-                    if ((this._note - other._note) == -2)
-                    {
-                        if (this._accidental - other._accidental == 1 ||
-                            this._accidental - other._accidental == -1 ||
-                            this._accidental - other._accidental == -2) return 1;
-                        // when its the same key!    
-                        if (this._accidental - other._accidental == 2) return 0;
-                    }
-                    //          B   precedes   A while comparing only!!! in Program.cs
-                    if ((this._note - other._note) == 2)
-                    {
-                        // when this.Note always precedes(KEY is on the RIGHT) the other.Note  
-                        if (this._accidental - other._accidental == 0 ||
-                            this._accidental - other._accidental == -1 ||
-                            this._accidental - other._accidental == 1 ||
-                            this._accidental - other._accidental == 2) return -1;
-                        // when its the same key!    
-                        if (this._accidental - other._accidental == -2) return 0;
-
-                    }
-                }
-                else Console.WriteLine("Cannot compare non existing Key");
-            }
-            return -1;
-        }
-        private readonly int ComparingTwoNotesFromNeighbourOctaves(Key other)
-        {
-            //                  B  from lower octave  follows C         
-            if (this._note == ENote.B && other._note == ENote.C ||
-                // or when I compare C   from upper octave to  B
-                this._note == ENote.C && other._note == ENote.B)
-            {
-                if (this._accidental - other._accidental == 0 ||
-                    this._accidental - other._accidental == -2 ||
-                    this._accidental == EAccidental.Flat) return 1;
-                if (this._accidental - other._accidental == 1 ||
-                    this._accidental - other._accidental == -1) return 0;
-            }
-            return 1;
-        }
-        private readonly int ComparingTwoNotesWithBlackKeyBetweenThem(Key other)
-        {
-            //           C   follows   D
-            if ((this._note - other._note) == -2)
-            {
-                if (this._accidental - other._accidental == 1 ||
-                    this._accidental - other._accidental == -1 ||
-                    this._accidental - other._accidental == -2) return 1;
-                // when its the same key!    
                 if (this._accidental - other._accidental == 2) return 0;
             }
-            //          D   precedes   C while comparing only!!! in Program.cs
-            if ((this._note - other._note) == 2)
-            {
-                // when this.Note always precedes(KEY is on the RIGHT) the other.Note  
-                if (this._accidental - other._accidental == 0 ||
-                    this._accidental - other._accidental == -1 ||
-                    this._accidental - other._accidental == 1 ||
-                    this._accidental - other._accidental == 2) return -1;
-                // when its the same key!    
-                if (this._accidental - other._accidental == -2) return 0;
-            }
-            return -1;
-        }
-        private readonly int ComparingTwoNotes(Key other)
-        {
-            //          E    follows   F
-            if ((this._note - other._note) == -1)
-            {
-                if (this._accidental - other._accidental == -2 ||
-                    this._accidental - other._accidental == -1 ||
-                    this._accidental - other._accidental == 0) return 1;
-                // when its the same key!
-                if (this._accidental - other._accidental == 1) return 0;
-                //when this.Note always precedes(KEY is on the RIGHT) the other.Note
-                if (this._accidental - other._accidental == 2) return -1;
-            }
-            //          F   precedes   E while comparing only!!! in Program.cs
-            if ((this._note - other._note) == 1)
-            {
-                if (this._accidental - other._accidental == -2 ||
-                    this._accidental - other._accidental == 0) return 1;
-                if (this._accidental - other._accidental == -1) return 0;
-                if (this._accidental - other._accidental == 1) return -1;
+            else Console.WriteLine("Cannot compare non existing Key");
 
-            }
-            return -1;
-        }
-        private readonly int ComparingTwoSameNotes(Key other)
-        {
-            //         D  is the same  D
-            if ((this._note - other._note) == 0)
-            {
-                //when this.Note always follows(KEY is on the LEFT) the other.Note
-                if (this._accidental - other._accidental == -1 ||
-                    this._accidental - other._accidental == -2) return 1;
-                //when this.Note always precedes(KEY is on the RIGHT) the other.Note
-                if (this._accidental - other._accidental == 1 ||
-                    this._accidental - other._accidental == 2) return -1;
-                // when its the same key!
-                return 0;
-            }
-            return 0;
+            return 1;
         }
         public override bool Equals(object obj)
         {
--- Task1.PianoKeyboard/Program.cs
+++ Task1.PianoKeyboard/Program.cs
@@ -1,20 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Task1.PianoKeyboard.Entities;
+﻿using Task1.PianoKeyboard.Entities;
 using Task1.PianoKeyboard.Enums;
 
-//var C = new Key(ENote.B, EAccidental.NoSign, EOctave.Fourth);
-//var D = new Key(ENote.C, EAccidental.Flat, EOctave.Fifth);
-
-//C.ToString();
-//D.ToString();
-//Console.WriteLine(C);
-//Console.WriteLine(D);
-//Console.WriteLine(C.CompareTo(D));
-
-//Console.WriteLine(C.Equals(D));
-//Refactoring
-var C_Sharp = new Key(ENote.B, EAccidental.NoSign, EOctave.Second);
-var D_Flat = new Key(ENote.C, EAccidental.Flat, EOctave.Third);
+var C_Sharp = new Key(ENote.C, EAccidental.Sharp, EOctave.First);
+var D_Flat = new Key(ENote.D, EAccidental.Flat, EOctave.First);
 
 C_Sharp.ToString();
 D_Flat.ToString();
