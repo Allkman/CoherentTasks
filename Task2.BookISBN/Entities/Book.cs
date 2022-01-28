df700abaf8aba643e0ac12dab23241086e85bca9
Allkman <algirdas.cern@gmail.com>
2022-01-28 12:59:58 +0200

    HW5 init commit

---
+++ Task2.BookISBN/Entities/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.BookISBN.Entities;
+
+namespace Task2.Entities.BookISBN
+{
+    internal class Book
+    {
+        public string Title { get => _title; set => _title= value; }
+        private string _title;
+        public DateTime? PublicationDate { get; set; }
+        public List<Author> Authors { get; set; }
+
+        public Book(string title)
+        {
+            _title = title;
+        }
+    }
+}
---
+++ Task2.BookISBN/Program.cs
@@ -0,0 +1,6 @@
+﻿using Task2.Entities.BookISBN;
+
+Console.WriteLine("");
+var book = new Book("CLR via C#");
+Console.WriteLine("Add new book to catalog");
+Console.ReadLine();
